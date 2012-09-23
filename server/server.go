971b2b50313949985be0de43a65c615a2d095d22
emepyc <emepyc@gmail.com>
2012-09-23 22:43:45 +0100

    Work on the scripts handler

--- server/server.go
+++ server/server.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 //	"net/url"
 	"log"
+	"os"
+	"os/exec"
+	"encoding/json"
 )
 
-
+func checkError (err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
@@ -15,14 +22,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func scriptHandler(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
+
 	log.Println("METHOD: ", r.Method)
 	log.Println("URL: ", r.URL)
+	log.Println("FRAGMENT: ", r.URL.Fragment)
+	log.Println("PATH: ", r.URL.Path)
 	log.Println("BODY: ", r.Body)
-	log.Println("URL: ", r.Form)
+	log.Println("URL2: ", r.Form)
 	log.Println("FORM: ", r.Form.Get("url"))
+
+	fname := ".." + r.URL.Path
+	args, err := json.Marshal(r.Form)
+	log.Printf("ARGS in Go side: %s", args)
+	checkError(err)
+	fh, err := os.Open(fname)
+	checkError(err)
+	defer fh.Close()
+
+	cmd := exec.Command(fname, string(args))
+	resp, err := cmd.Output()
+	checkError(err)
+	log.Printf("RESP: %s", resp)
+	fmt.Fprintf(w, string(resp))
+	
 }
 
 func main() {
