355bdf6dc6b0e62b6752922d20ba8534b1e2f73a
emepyc <emepyc@gmail.com>
2012-09-23 22:41:22 +0100

    callint test.pl in ajax test

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,11 @@
 // wait for the DOM to be loaded 
 $(document).ready(function() { 
     $("#Connect").click(function() {
-	$.ajax({url        : "/scripts/db_connect.pl",
+	$.ajax({url        : "/scripts/test.pl",
 		type       : "post",
-		beforeSend : beforeFunc,
+//		beforeSend : beforeFunc,
 		data       : "url=" + $("#db_url").val(),
+//		data       : "url=" + $("#db_url").serialize(),
 		success    : function(res) {
 		    $("#connexion_msg").html(res);
 		}});
