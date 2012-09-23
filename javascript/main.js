421d49462f1e694c6bc8951bc651c5eca3afec8f
emepyc <emepyc@gmail.com>
2012-09-23 18:32:47 +0100

    Fixed simple ajax code

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,11 @@
 
-
-
 // wait for the DOM to be loaded 
 $(document).ready(function() { 
     $("#Connect").click(function() {
-	var url = $("#db_url").val()
-	$.ajax({url        : "/scripts/db_connect.pl" + "#" + url,
-		method     : "post",
+	$.ajax({url        : "/scripts/db_connect.pl",
+		type       : "post",
 		beforeSend : beforeFunc,
+		data       : "url=" + $("#db_url").val(),
 		success    : function(res) {
 		    $("#connexion_msg").html(res);
 		}});
@@ -15,11 +13,6 @@ $(document).ready(function() {
 
 }); 
 
-// function call_msg() {
-//     beforeSend : beforeFunc,
-//     success    : successFunc
-// }
-
 function beforeFunc() {
-    alert("sending ajax request...\n");
+    alert("url=" + $("#db_url").val())
 }
