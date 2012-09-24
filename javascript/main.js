c365fff9a64bdc512950a8a539eeaba64d84b892
emepyc <emepyc@gmail.com>
2012-09-24 22:44:01 +0100

    parse json in db connexion

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     $("#Connect").click(function() {
 	$.ajax({url        : "/scripts/db_connect.pl",
 		type       : "post",
-//		beforeSend : beforeFunc,
 		data       : "url=" + $("#db_url").val(),
-//		data       : "url=" + $("#db_url").serialize(),
 		success    : function(res) {
-		    $("#connexion_msg").html(res);
+		    var resObj = JSON.parse(res)
+		    $("#connexion_msg").html(resObj.status);
+		    $("#pipeline_diagram").html(resObj.analyses);
 		}});
     });
 
