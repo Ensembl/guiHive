0d0f204d5baad96092d69bccca60552724223275
emepyc <emepyc@gmail.com>
2012-09-28 17:16:06 +0100

    new ajax call to update parameters

--- javascript/main.js
+++ javascript/main.js
@@ -8,40 +8,60 @@ $(document).ready(function() {
 		type       : "post",
 		data       : "url=" + $("#db_url").val(),
 		dataType   : "json",
-		beforeSend : onSend_dbconnect,
-		success    : onSuccess
+		beforeSend : onSend_dbConnect,
+		success    : onSuccess_dbConnect
 	       });
     });
 
 }); 
 
 // res is the JSON-encoded response from the server in the Ajax call
-function onSuccess(res) {
+function onSuccess_dbConnect(res) {
     $("#connexion_msg").html(res.status);
     $("#pipeline_diagram").html(res.analyses);
     url = $("#db_url").val();
     $(".analysis_link").click(function() {
 	$.ajax({url        : "/scripts/db_fetch_analysis.pl",
-//		beforeSend : onSend,
 		type       : "post",
-		dataType   : "json",
 		data       : "url=" + url + "&logic_name=" + this.id,
-		success    : function(resp) {
-		    if (resp.status == "ok") {
-			$("#analysis_details").html(resp.analysis_info);	
-		    } else {
-			$("#connexion_msg").html(resp.status);
-		    }
-		}
+		dataType   : "json",
+		success    : onSuccess_fetchAnalysis
 	       });
     });
 }
 
-function onSend_dbconnect() {
-//    $('#connexion_msg').html('<img src="http://static.tumblr.com/d0qlne1/qVol4tb08/loading.gif" />');
+// res is the JSON-encoded response from the server in the Ajax call
+function onSuccess_fetchAnalysis(analysisRes) {
+    if(analysisRes.status == "ok") {
+	$("#analysis_details").html(analysisRes.analysis_info);
+    } else {
+	$("#connexion_msg").html(analysisRes.status);
+    }
+    $(".set_val").change(function() {
+	$.ajax({url        : "/scripts/db_update_analysis.pl",
+		type       : "post",
+		data       : "url=" + url + "&newval=" + this.value + "&column_name=" + this.id + "&analysis_id=" + this.name + "&action=" + $(this).attr("class"),
+		dataType   : "json",
+		beforeSend : onSend_dbUpdate,
+		success    : function(updateRes) {
+		    if(updateRes.status != "ok") {
+			alert(updateRes.status);
+		    }
+		    complete: onSuccess_fetchAnalysis(analysisRes);
+		}
+	       });
+    });
+}
+			    
+function onSend_dbConnect() {
     $('#connexion_msg').html('<img src="../images/preloader.gif" width="40px" height="40px"/>');
+}
+
+function onSend_dbUpdate() {
+    $('#analysis_details').html('<img src="../images/preloader.gif" width="40px" height="40px"/>');
 }
 
 function onSend(req, settings) {
     alert(JSON.stringify(this));
 }
+
