a1e0431ed5bc4c58d819ac876075a6d504e040d6
emepyc <emepyc@gmail.com>
2012-10-01 01:10:19 +0100

    Factor out of update_db function

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,8 @@ var url = "";
 
 // wait for the DOM to be loaded 
 $(document).ready(function() { 
+    // Default value. Only for testing. TODO: Remove the following line
+    $("#db_url").val("mysql://ensro@127.0.0.1:2912/mp12_compara_nctrees_69b");
     $("#Connect").click(function() {
 	$.ajax({url        : "/scripts/db_connect.pl",
 		type       : "post",
@@ -18,7 +20,7 @@ $(document).ready(function() {
 // res is the JSON-encoded response from the server in the Ajax call
 function onSuccess_dbConnect(res) {
     $("#connexion_msg").html(res.status);
-    $("#pipeline_diagram").html(res.analyses);
+    $("#pipeline_diagram").html(res.out_msg);
     url = $("#db_url").val();
     $(".analysis_link").click(function() {
 	$.ajax({url        : "/scripts/db_fetch_analysis.pl",
@@ -33,24 +35,26 @@ function onSuccess_dbConnect(res) {
 // res is the JSON-encoded response from the server in the Ajax call
 function onSuccess_fetchAnalysis(analysisRes) {
     if(analysisRes.status == "ok") {
-	$("#analysis_details").html(analysisRes.analysis_info);
+	$("#analysis_details").html(analysisRes.out_msg);
     } else {
 	$("#connexion_msg").html(analysisRes.status);
     }
-    $(".set_val").change(function() {
-	$.ajax({url        : "/scripts/db_update_analysis.pl",
-		type       : "post",
-		data       : "url=" + url + "&newval=" + this.value + "&column_name=" + this.id + "&analysis_id=" + this.name + "&action=" + $(this).attr("class"),
-		dataType   : "json",
-		beforeSend : onSend_dbUpdate,
-		success    : function(updateRes) {
-		    if(updateRes.status != "ok") {
-			alert(updateRes.status);
-		    }
-		    complete: onSuccess_fetchAnalysis(analysisRes);
+    $(".set_val").change(analysisRes, update_db);
+    $(".delete_param").click(analysisRes, update_db);
+}
+
+function update_db(e) {
+    $.ajax({url        : "/scripts/db_update_analysis.pl",
+	    type       : "post",
+	    data       : "url=" + url + "&newval=" + this.value + "&column_name=" + this.id + "&analysis_id=" + this.name + "&action=" + $(this).attr("class"),
+	    dataType   : "json",
+	    success    : function(updateRes) {
+		if(updateRes.status != "ok") {
+		    alert(updateRes.status);
 		}
-	       });
-    });
+		complete: onSuccess_fetchAnalysis(e.data);
+	    }
+	   });
 }
 			    
 function onSend_dbConnect() {
