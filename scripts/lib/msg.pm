a7be4fc9e9091b2df7d6e81bb3a6b7da5d754b79
emepyc <emepyc@gmail.com>
2012-10-01 01:09:22 +0100

    Perl's libraries

---
+++ scripts/lib/msg.pm
@@ -0,0 +1,55 @@
+package msg;
+
+use strict;
+use warnings;
+use Data::Dumper;
+use JSON::XS;
+
+sub new {
+    my ($class) = @_;
+    my $self = bless({}, $class);
+    $self->init();
+    return $self;
+}
+
+sub init {
+    my ($self) = @_;
+    $self->err_msg("");
+    $self->status("ok");
+    $self->out_msg("");
+    return;
+}
+
+sub err_msg {
+    my ($self, $msg) = @_;
+    $self->{err_msg} = $msg if (defined $msg);
+    return $self->{err_msg};
+}
+
+sub status {
+    my ($self, $status) = @_;
+    $self->{status} = $status if (defined $status);
+    return $self->{status};
+}
+
+sub out_msg {
+    my ($self, $out_msg) = @_;
+    $self->{out_msg} = $out_msg if (defined $out_msg);
+    return $self->{out_msg};
+}
+
+sub TO_JSON {
+    my ($self) = @_;
+    return { %$self };
+}
+
+sub toJSON {
+    my ($self) = @_;
+    return JSON::XS->
+	new->
+	indent(0)->
+	allow_blessed->
+	convert_blessed->encode($self);
+}
+
+1;
