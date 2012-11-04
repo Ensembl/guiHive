6905d4428ba251091ff22bf31b55c48f06721d29
emepyc <emepyc@gmail.com>
2012-11-04 12:47:06 +0000

    old db_monitor_analysis.pl is substituted by the newest (pie-oriented) one

--- scripts/db_monitor_analysis.pl
+++ scripts/db_monitor_analysis.pl
@@ -4,16 +4,24 @@ use strict;
 use warnings;
 
 use Bio::EnsEMBL::Hive::URLFactory;
+use Bio::EnsEMBL::Hive::Utils::Graph;
 use JSON::XS;
-use HTML::Template;
-use Data::Dumper; ## needed?
+use Data::Dumper;
 
-use lib ("./scripts/lib"); ## Only needed for testing the script.
-use new_hive_methods; # needed?
+use lib ("./scripts/lib");  # Only needed for testing the script
+use new_hive_methods;   # needed?
 use msg;
 
+## colors encoding the different job status we may have
+my $job_colors = {
+		  'semaphored' => 'yellow',
+		  'ready'      => 'cyan',
+		  'inprogress' => 'blue',
+		  'failed'     => 'red',
+		  'done'       => 'green',
+		 };
+
 my $json_data = shift @ARGV || '{"url":["mysql://ensadmin:ensembl@127.0.0.1:2912/mp12_compara_nctrees_69d"], "analysis_id":["22"]}';
-my $monitor_template = $ENV{GUIHIVE_BASEDIR} . "static/analysis_monitor.html";
 
 my $var         = decode_json($json_data);
 my $url         = $var->{url}->[0];
@@ -23,36 +31,62 @@ my $dbConn = Bio::EnsEMBL::Hive::URLFactory->fetch($url);
 my $response = msg->new();
 
 if (defined $dbConn) {
-  my $analysis_stats;
-  eval {
-    $analysis_stats = $dbConn->get_AnalysisStatsAdaptor->fetch_by_analysis_id($analysis_id);
-  };
-  if ($@) {
-    $response->err_msg("I can't retrieve the analysis_stats with id $analysis_id: $@");
-    $response->status("FAILED");
-  }
-  if (!defined $analysis_stats) {
-    $response->err_msg("I can't retrieve analysis_stats with id $analysis_id from the database");
-    $response->status("FAILED");
-  }
-  $response->out_msg(formMonitorInfo($analysis_stats));
+    my $analysis_stats;
+    eval {
+	$analysis_stats = $dbConn->get_AnalysisStatsAdaptor->fetch_by_analysis_id($analysis_id);
+    };
+    if ($@) {
+	$response->err_msg("In can't retrieve the analysis_stats with id $analysis_id: $@");
+	$response->status("FAILED");
+    }
+    if (!defined $analysis_stats) {
+	$response->err_msg("I can't retrieve analysis_stats with id $analysis_id from the database");
+	$response->status("FAILED");
+    }
+    $response->out_msg(formMonitorInfo($analysis_stats));
 } else {
-  $response->err_msg("The provided URL seems to be invalid. Please check the URL and try again");
-  $response->status("FAILED");
+    $response->err_msg("The provided URL seems to be invalid. Please check it and try again");
+    $response->status("FAILED");
 }
 
 print $response->toJSON;
 
 sub formMonitorInfo {
   my ($analysis_stats) = @_;
-  my $template = HTML::Template->new(filename => $monitor_template);
-  $template->param( 'jobs_total'      => $analysis_stats->total_job_count(),
-		    'jobs_done'       => $analysis_stats->done_job_count(),
-		    'jobs_failed'     => $analysis_stats->failed_job_count(),
-		    'jobs_ready'      => $analysis_stats->ready_job_count(),
-		    'jobs_semaphored' => $analysis_stats->semaphored_job_count(),
-		    'jobs_running'    => $analysis_stats->inprogress_job_count(),
-		    'jobs_remaining'  => $analysis_stats->remaining_job_count(),
-			   );
-  return $template->output();
+  my $config = Bio::EnsEMBL::Hive::Utils::Config->new();
+  my $status = $analysis_stats->status();
+  my $status_colour = $config->get('Graph', 'Node', $analysis_stats->status, 'Colour');
+  my $full_status = {status => $status_colour,
+		     total  => $analysis_stats->total_job_count(),
+		     jobs   => {
+				counts => [],
+				colors => [],
+			       },
+		    };
+
+  for my $job_status (qw/semaphored ready inprogress failed done/) {
+    my $method_name = ${job_status} . "_job_count";
+    my $count = $analysis_stats->$method_name;
+      push @{$full_status->{jobs}->{counts}}, $count+0;
+      push @{$full_status->{jobs}->{colors}}, $job_colors->{$job_status};
+  }
+
+  # We can't have all zeros
+  if (sum($full_status->{jobs}->{counts})) {
+    push @{$full_status->{jobs}->{counts}}, 0;
+  } else {
+    push @{$full_status->{jobs}->{counts}}, 1;
+  }
+  push @{$full_status->{jobs}->{colors}}, "white";
+
+  return $full_status;
+}
+
+sub sum {
+  my ($arr) = @_;
+  my $res = 0;
+  for my $i (@$arr) {
+    $res+=$i;
+  }
+  return $res;
 }
