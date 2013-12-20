1e8082ad62c517d626a8de89c27f7d24fec00843
Miguel Pignatelli <mp@ebi.ac.uk>
2013-12-20 16:59:30 +0000

    Licensing of all our files

--- server/release.go
+++ server/release.go
@@ -1,6 +1,3 @@
-// Copyright 2012 Miguel Pignatelli. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
 
 // +build !debug
 
