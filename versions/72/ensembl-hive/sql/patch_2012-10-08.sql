fc1c8c079fc7ed43702f5a952a287c3afce99a92
Matthieu Muffato <muffato@ebi.ac.uk>
2015-11-18 11:06:34 +0000

    Moved all the files related to the database-version 56

--- versions/72/ensembl-hive/sql/patch_2012-10-08.sql
+++
@@ -1,6 +0,0 @@
-
-# replace unique keys with proper primary keys in two tables (BaseAdaptor needs this) :
-
-ALTER TABLE analysis_stats DROP KEY `analysis_id`, ADD PRIMARY KEY (analysis_id);
-ALTER TABLE job_file DROP KEY `job_retry`, ADD PRIMARY KEY (job_id, retry);
-
--- versions/77/ensembl-hive/sql/patch_2012-11-09.sql
+++
@@ -1,3 +0,0 @@
-    # varchar(80) was too limiting, so let's extend it:
-
-ALTER TABLE worker MODIFY COLUMN log_dir varchar(255) DEFAULT NULL;
