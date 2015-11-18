070ff50352dc68ee38167c40e8c8bf140241718a
Matthieu Muffato <muffato@ebi.ac.uk>
2015-11-18 10:26:45 +0000

    No version-specific files on the "server" branch

--- versions/56/ensembl-hive/sql/patch_2007-11-16.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE analysis_stats ADD COLUMN max_retry_count int(10) DEFAULT 3 NOT NULL AFTER done_job_count;
-ALTER TABLE analysis_stats ADD COLUMN failed_job_tolerance int(10) DEFAULT 0 NOT NULL AFTER max_retry_count;
--- versions/62/ensembl-hive/sql/patch_2009-12-15.sql
+++
@@ -1,5 +0,0 @@
-# adding simple counting semaphores:
-
-ALTER TABLE analysis_job ADD COLUMN semaphore_count   int(10) NOT NULL default 0;
-ALTER TABLE analysis_job ADD COLUMN semaphored_job_id int(10) DEFAULT NULL;
-
