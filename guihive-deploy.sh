cd71ec730e96d277e9e0651823d5e26b0f9a365b
ens-bwalts <bwalts@.ebi.ac.uk>
2016-10-06 16:46:44 +0100

    added support for schema versions through 83

--- guihive-deploy.sh
+++ guihive-deploy.sh
@@ -85,6 +85,7 @@ add_guihive_version "56" "db_version/56" "version/2.0"
 add_guihive_version "62" "db_version/62" "version/2.2"
 add_guihive_version "73" "db_version/73" "version/2.3"
 add_guihive_version "80" "db_version/80" "version/2.4"
+add_guihive_version "83" "db_version/80" "master"
 
 # $db_version  $aliased_db_version
 link_guihive_version "63" "62"
@@ -103,6 +104,8 @@ link_guihive_version "76" "80"
 link_guihive_version "77" "80"
 link_guihive_version "78" "80"
 link_guihive_version "79" "80"
+link_guihive_version "81" "83"
+link_guihive_version "82" "83"
 
 trap - EXIT
 
