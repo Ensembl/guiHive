7a8082b7c66a2b97d0cb097c46b1f30aabf5b799
Matthieu Muffato <muffato@ebi.ac.uk>
2017-01-27 17:11:20 +0000

    Reset FAILED jobs

--- scripts/lib/hive_extended.pm
+++ scripts/lib/hive_extended.pm
@@ -351,8 +351,9 @@ use Bio::EnsEMBL::Hive::DBSQL::BaseAdaptor;
 *Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::reset_jobs_for_analysis_id_and_sync = sub {
   my ($self, $analysis_id) = @_;
 
-  $self->reset_jobs_for_analysis_id($analysis_id, ['FAILED']);
-  $self->db->get_Queen()->synchronize_AnalysisStats($self->db->get_AnalysisStatsAdaptor->fetch_by_analysis_id($analysis_id));
+  my $analysis = $self->db->hive_pipeline->collection_of('Analysis')->find_one_by('dbID', $analysis_id);
+  $self->reset_jobs_for_analysis_id([$analysis], ['FAILED']);
+  $self->db->get_Queen()->synchronize_AnalysisStats($analysis->stats);
 
   return;
 };
